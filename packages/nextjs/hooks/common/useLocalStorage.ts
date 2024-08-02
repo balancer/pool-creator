5b1f74a55badea5a42344c5e249ca65c6ed5aedb
Matthew Pereira <matthewdavidpereira@gmail.com>
2024-08-02 13:27:49 -0700

    troubleshoot accessing localstorage within nextjs

--- packages/nextjs/hooks/common/useLocalStorage.ts
+++ packages/nextjs/hooks/common/useLocalStorage.ts
@@ -1,13 +1,15 @@
+"use client";
+
 import { useEffect, useState } from "react";
 
 export const useLocalStorage = <T>(key: string, defaultValue: T) => {
   const [state, setState] = useState<T>(() => {
-    const savedValue = localStorage.getItem(key);
+    const savedValue = window.localStorage.getItem(key);
     return savedValue ? JSON.parse(savedValue) : defaultValue;
   });
 
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(state));
+    window.localStorage.setItem(key, JSON.stringify(state));
   }, [key, state]);
 
   return [state, setState] as const;
