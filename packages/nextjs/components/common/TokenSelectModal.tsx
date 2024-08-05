c4361414efd367b6a171aaeeeea37290a09c35ab
Daniel <danielBeets@protonmail.com>
2024-08-05 09:29:16 +0200

    close modal on bg click

--- packages/nextjs/components/common/TokenSelectModal.tsx
+++ packages/nextjs/components/common/TokenSelectModal.tsx
@@ -10,7 +10,7 @@ type ModalProps = {
 };
 export const TokenSelectModal: React.FC<ModalProps> = ({ tokenOptions, setIsModalOpen, setToken }) => {
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50">
+    <div className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50" onClick={() => setIsModalOpen(false)}>
       <div className="relative w-[500px] p-7">
         <div className="relative bg-base-300 border border-base-200 rounded-lg p-7 h-[500px] overflow-y-auto">
           <XMarkIcon
