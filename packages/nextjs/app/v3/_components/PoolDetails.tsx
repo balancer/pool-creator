d03fa11350fa13d7c2251cbd552a2c4cfcca4861
Matthew Pereira <matthewdavidpereira@gmail.com>
2024-12-04 17:20:23 -0800

    remove not needed details from pool preview

--- packages/nextjs/app/v3/_components/PoolDetails.tsx
+++ packages/nextjs/app/v3/_components/PoolDetails.tsx
@@ -285,23 +285,6 @@ function TokenDetails({ token }: { token: TokenConfig }) {
               </a>
             </div>
           </div>
-          <div className="flex gap-1 mt-1 ml-3">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m16.49 12 3.75 3.75m0 0-3.75 3.75m3.75-3.75H3.74V4.499"
-              />
-            </svg>
-            Pays Yield Fees :<div>{token.paysYieldFees ? "true" : "false"}</div>
-          </div>
         </>
       )}
     </div>
