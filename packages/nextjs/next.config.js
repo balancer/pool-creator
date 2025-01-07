44857f2dcc23daf6b2e1cfea49835d88fb770081
Alberto Gualis <agualis@gmail.com>
2025-01-07 10:10:18 +0100

    feat: adapt manifest to work with Safe App

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -14,6 +14,36 @@ const nextConfig = {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
+  // Safe App setup
+  headers: manifestHeaders,
 };
 
 module.exports = nextConfig;
+
+/**
+ * Add specific CORS headers to the manifest.json file
+ * This is required to allow the Safe Browser to fetch the manifest file
+ * More info: https://help.safe.global/en/articles/40859-add-a-custom-safe-app
+ */
+async function manifestHeaders() {
+  const corsHeaders = [
+    {
+      key: "Access-Control-Allow-Origin",
+      value: "*",
+    },
+    {
+      key: "Access-Control-Allow-Methods",
+      value: "GET",
+    },
+    {
+      key: "Access-Control-Allow-Headers",
+      value: "X-Requested-With, content-type, Authorization",
+    },
+  ];
+  return [
+    {
+      source: "/manifest.json",
+      headers: corsHeaders,
+    },
+  ];
+}
