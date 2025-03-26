730793c961bb850bbe20a4be82bf008808cacd3e
franz <franzns11@gmail.com>
2025-03-26 10:48:21 +0100

    increase poolname lenght

--- packages/nextjs/utils/constants.ts
+++ packages/nextjs/utils/constants.ts
@@ -5,5 +5,5 @@ export const COW_MIN_AMOUNT = BigInt(1e6);
  * @dev Gauge creation reverts if the symbol is longer than 26 characters (Franz said so)
  * https://github.com/balancer/pool-creator/issues/17#issuecomment-2430158673
  */
-export const MAX_POOL_NAME_LENGTH = 32;
+export const MAX_POOL_NAME_LENGTH = 64;
 export const MAX_POOL_SYMBOL_LENGTH = 26;
