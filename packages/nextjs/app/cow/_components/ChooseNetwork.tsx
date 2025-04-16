3b9a0e892bdac9299650bf5a1b7843a45c19977f
Matt Pereira <mattpereira.dev@gmail.com>
2025-04-16 09:24:18 -0700

    improve choose network styles;

--- packages/nextjs/app/cow/_components/ChooseNetwork.tsx
+++ packages/nextjs/app/cow/_components/ChooseNetwork.tsx
@@ -6,15 +6,15 @@ export const ChooseNetwork = ({ options }: { options: chains.Chain[] }) => {
 
   return (
     <div className="bg-base-200 rounded-xl px-5 pt-5 pb-7 w-96">
-      <div className="text-2xl text-center mb-4">Supported Networks</div>
-      <div className="flex flex-col gap-3 px-5">
+      <div className="text-2xl text-center mb-4">Choose a Network</div>
+      <div className="flex flex-col gap-4 px-5">
         {options.map(network => (
           <div
             key={network.id}
             onClick={() => {
               switchChain?.({ chainId: network.id });
             }}
-            className="text-lg btn bg-base-100 w-full rounded-xl"
+            className="text-xl btn btn-lg bg-base-100 w-full rounded-xl"
           >
             {network.name}
           </div>
