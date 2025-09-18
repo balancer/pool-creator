bde577a85ccccc3195f594cf1479c877cc1a38df
Matt Pereira <mattpereira.dev@gmail.com>
2025-09-18 14:56:32 -0700

    add support for plasma

--- packages/nextjs/utils/supportedNetworks.ts
+++ packages/nextjs/utils/supportedNetworks.ts
@@ -1,8 +1,17 @@
-import { hyperEVM } from "./constants";
+import { hyperEVM, plasma } from "./constants";
 import * as chains from "viem/chains";
 
 export const supportedNetworks = {
-  balancerV3: [chains.mainnet, chains.arbitrum, chains.base, hyperEVM, chains.gnosis, chains.avalanche, chains.sepolia],
+  balancerV3: [
+    chains.mainnet,
+    chains.arbitrum,
+    chains.base,
+    chains.gnosis,
+    chains.avalanche,
+    hyperEVM,
+    plasma,
+    chains.sepolia,
+  ],
   beets: [chains.sonic, chains.optimism],
   cowAmm: [chains.mainnet, chains.arbitrum, chains.base, chains.gnosis, chains.sepolia],
 };
