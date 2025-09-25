da020e230b1373d4e5618eb524a7fc20bc51a9f1
Matt Pereira <mattpereira.dev@gmail.com>
2025-09-25 07:47:42 -0700

    Add support for plasma chain (#78)
    * add support for plasma
    
    * hide ECLP on plasma

--- packages/nextjs/app/v3/_components/PoolConfiguration/ChooseType/index.tsx
+++ packages/nextjs/app/v3/_components/PoolConfiguration/ChooseType/index.tsx
@@ -3,13 +3,13 @@ import { PoolTypeButton } from "./PoolTypeButton";
 import { type SupportedPoolTypes, poolTypeMap } from "~~/utils/constants";
 
 export function ChooseType() {
-  const poolTypes = Object.keys(poolTypeMap).slice(0, 4) as SupportedPoolTypes[];
+  const poolTypes = Object.keys(poolTypeMap).slice(0, 3) as SupportedPoolTypes[];
 
   return (
     <>
       <div className="flex flex-col justify-center h-full gap-10 mt-10">
         <div className="text-xl">Choose a pool type:</div>
-        <div className="grid grid-cols-2 gap-4 ">
+        <div className="grid grid-cols-3 gap-4 ">
           {poolTypes.map((type: SupportedPoolTypes) => (
             <PoolTypeButton key={type} selectedPoolType={type} />
           ))}
